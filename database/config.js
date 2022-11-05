786c8609b0ecf55940ea37e7c7da0d1763d9b10f
lestherxm <lestherxm@gmail.com>
2022-11-05 02:14:50 -0600

    fixing heroku credentials on Pool.js

--- database/config.js
+++ database/config.js
@@ -1,10 +1,9 @@
 module.exports = {
     db: {
-        user: 'root',
-        password: 'root',
-        host: 'localhost',
+        user: 'root' || 'cqqvfxwlckurku',
+        password: 'root' || '3f77cda980d775a154f76b10cefa19de2a430ac3c313a6a30f418549b23d4d4d',
+        host: 'localhost' || 'ec2-35-170-21-76.compute-1.amazonaws.com',
         port: 5432,
-        database: 'efdw2022_backend',
+        database: 'efdw2022_backend' || 'd4v06dr02eqtda'
     }
 }
-
--- database/pool.js
+++ database/pool.js
@@ -2,13 +2,14 @@ const { Pool } = require("pg");
 const { db } = require('./config');
 
 const pool = new Pool({
-    user: db.user || 'cqqvfxwlckurku',
-    password: db.password || '3f77cda980d775a154f76b10cefa19de2a430ac3c313a6a30f418549b23d4d4d',
-    host: db.host || 'ec2-35-170-21-76.compute-1.amazonaws.com',
-    port: db.port || 5432,
-    database: db.database ||  'd4v06dr02eqtda',
+    user: db.user,
+    password: db.password,
+    host: db.host,
+    port: db.port,
+    database: db.database,
     ssl: {
         rejectUnauthorized: false
     }
 });
+
 module.exports = pool;
