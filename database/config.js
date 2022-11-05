393b078e861785407e5d1b1b4ef3b719c9fd132d
lestherxm <lestherxm@gmail.com>
2022-11-05 02:16:29 -0600

    fixing heroku credentials on Pool.js

--- database/config.js
+++ database/config.js
@@ -1,9 +1,19 @@
+// module.exports = {
+//     db: {
+//         user: 'root' || 'cqqvfxwlckurku',
+//         password: 'root' || '3f77cda980d775a154f76b10cefa19de2a430ac3c313a6a30f418549b23d4d4d',
+//         host: 'localhost' || 'ec2-35-170-21-76.compute-1.amazonaws.com',
+//         port: 5432,
+//         database: 'efdw2022_backend' || 'd4v06dr02eqtda'
+//     }
+// }
+
 module.exports = {
     db: {
-        user: 'root' || 'cqqvfxwlckurku',
-        password: 'root' || '3f77cda980d775a154f76b10cefa19de2a430ac3c313a6a30f418549b23d4d4d',
-        host: 'localhost' || 'ec2-35-170-21-76.compute-1.amazonaws.com',
+        user: 'cqqvfxwlckurku',
+        password: '3f77cda980d775a154f76b10cefa19de2a430ac3c313a6a30f418549b23d4d4d',
+        host: 'ec2-35-170-21-76.compute-1.amazonaws.com',
         port: 5432,
-        database: 'efdw2022_backend' || 'd4v06dr02eqtda'
+        database: 'd4v06dr02eqtda'
     }
 }
