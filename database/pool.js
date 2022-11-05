de25765459501e06e44a90eefe86eaf051632b2a
lestherxm <lestherxm@gmail.com>
2022-11-05 01:58:40 -0600

    fixing heroku credentials on Pool.js

--- database/pool.js
+++ database/pool.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 const { db } = require('./config');
 
 const pool = new Pool({
-    user: db.user,
-    password: db.password,
-    host: db.host,
+    user: db.user || 'cqqvfxwlckurku',
+    password: db.password || '3f77cda980d775a154f76b10cefa19de2a430ac3c313a6a30f418549b23d4d4d',
+    host: db.host || 'ec2-35-170-21-76.compute-1.amazonaws.com',
     port: db.port,
-    database: db.database,
+    database: db.database ||  'd4v06dr02eqtda',
     ssl: {
         rejectUnauthorized: false
     }
